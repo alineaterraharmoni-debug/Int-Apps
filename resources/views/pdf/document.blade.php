dff12138cc9f7a75841c1e95660e6d9b40a472ec
alineaterraharmoni-debug <alineaterraharmoni@gmail.com>
2026-08-31 18:18:52 +0700

    bug fix

--- resources/views/pdf/document.blade.php
+++ resources/views/pdf/document.blade.php
@@ -205,7 +205,7 @@
                 $totalHighlight = $doc->type === 'invoice' && $doc->taxes->isEmpty() && $doc->payment_scheme !== 'staged';
             @endphp
             <tr class="total-row">
-                <td colspan="{{ $colCount - 4 }}" style="border-top: 2px solid #000;"></td>
+                <td colspan="{{ $colCount }}" style="border-top: 2px solid #000;"></td>
                 <td style="text-align: right; {{ $totalHighlight ? 'background: #F3F4F6;' : '' }}">Total</td>
                 <td class="money" style="{{ $totalHighlight ? 'background: #F3F4F6;' : '' }}">@include('pdf.partials.money', ['amount' => $doc->total])</td>
             </tr>
